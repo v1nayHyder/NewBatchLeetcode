e4e41423c6109b9e0f27779b5b648fa59189f2bd
VinayPYadav <vinayprakashyadav8364@gmail.com>
2024-04-03 22:10:37 +0530

    again

--- src/main/java/New_batch_leetcode/Test.java
+++ src/main/java/New_batch_leetcode/Test.java
@@ -6,5 +6,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("I Started new batch Leetcode problem");
         System.out.println("Hello world");
+        System.out.println("hi");
     }
 }
