8a878dea39a41df5d6b6bf3ee83497be13f2f1c2
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-06-24 21:12:23 +0530

    use left=0 and top=0, and rigth=length of columns , bottom=length of rows

--- src/main/java/New_batch_leetcode/Test.java
+++ src/main/java/New_batch_leetcode/Test.java
@@ -1,23 +1,40 @@
 package New_batch_leetcode;
 
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Test {
-    public static void main(String[] args) {
-        String s="AmaAaRRurRpRXTXXTXX";
-        HashMap<Character,Integer> hashMap=new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-          if (Character.isUpperCase(s.charAt(i))){
-              hashMap.put(s.charAt(i),hashMap.getOrDefault(s.charAt(i),0)+1);
-          }
+    public  static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> mainList=new ArrayList<>();
+        String strs2[]=new String[strs.length];
+        for (int i = 0; i <strs.length ; i++) {
+            char ch[]=strs[i].toCharArray();
+            Arrays.sort(ch);
+            strs2[i]=new String(ch);
         }
-        for (char val:hashMap.keySet()){
-            if (hashMap.get(val)%2==0){
-                System.out.print(val+" ");
+        for (int i = 0; i <strs2.length ; i++) {
+            if (!strs2[i].equals("taken")) {
+                List<String> child=new ArrayList<>();
+                child.add(strs[i]);
+                for (int j = i + 1; j < strs.length; j++) {
+                    if (strs2[i].length()==strs2[j].length()&&strs2[i].equals(strs2[j])){
+                        child.add(strs[j]);
+                        strs2[j]="taken";
+                    }
+
+                }
+                System.out.println(Arrays.toString(strs2));
+
+                mainList.add(child);
             }
+
         }
+        System.out.println(Arrays.toString(strs2));
+        System.out.println(mainList);
+return mainList;
     }
+    public static void main(String[] args) {
+    List list=groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
+    }
+
 }
